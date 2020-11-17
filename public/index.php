f5ea22d7b076b1b0189c28ff78dccc496c0e4e9a
Pavel Chernyshev <vengador32@gmail.com>
2020-11-17 12:47:09 +0600

    Добавляю тестовый json ответ

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php
-header('Content-type: application/json; charset=utf-8');
-echo json_encode(['status' => 'ok', 'message' => 'Kolesa Academy!']);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response) {
+    $payload = json_encode(['status' => 'ok', 'message' => 'Kolesa Academy!']);
+    $response->getBody()->write($payload);
+
+    return $response
+        ->withHeader('Content-Type', 'application/json; charset=utf-8');
+});
+
+$app->run();
